5c17d8b5a22d116b4d41c8cd6c426718cf8e60ab
莫兮 <moxi@zhihu.com>
2013-03-12 02:33:59 +0800

    markdown

--- markdown/myflaskr_markdown/flaskr.py
+++ markdown/myflaskr_markdown/flaskr.py
@@ -4,10 +4,11 @@ from contextlib import closing
 import sqlite3
 import time
 from flask import Flask,request,session,g,redirect,url_for,\
-     abort,render_template,flash,Markup
+     abort,render_template,flash
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
+
 
 # configuration
 
@@ -61,19 +62,29 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-@app.template_filter('md')
-def md_filter(s):
-    return Markup(markdown.markdown(s))
+#def connect_db():
+#    return sqlite3.connect(app.config['DATABASE'])
+
+#def init_db():
+#    with closing(connect_db()) as db:
+#        with app.open_resource('schema.sql') as f:
+#	    db.cursor().executescript(f.read())
+#	db.commit() 
+
+#@app.before_request
+#def before_request():
+#    g.db = connect_db()
+
+#@app.teardown_request
+#def teardown_request(exception):
+#    g.db.close()
 
 @app.route('/')
-def show_entries():    
+def show_entries():
+#    cur = g.db.execute('select title,author,time,text from entries order by id desc')
     entries = Entry.query.all()
-    {% for entry in entries %}
-        {% entry.text | md %}
-    {% endfor %}
-    return render_template('show_entries.html',entries=entries) 
+    return render_template('show_entries.html', entries=entries)
 
-    
 @app.route('/about')
 def about():
     return render_template('about.html')
